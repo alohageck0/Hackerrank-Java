ce0c0d381831027d4e60972de93607e3da23dce4
Evgenii <royalfiish@gmail.com>
2016-02-03 09:00:38 -0800

    02/03/2016

--- StringPractice/Token/src/Solution.java
+++ StringPractice/Token/src/Solution.java
@@ -1,2 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+
 public class Solution {
+
+
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        //Complete the code
+
+    }
 }
