9ea6d71c85139d8b6a3a0397a184b7960baae600
alohageck0 <royalfiish@gmail.com>
2016-02-02 21:27:19 -0800

    02/02/2016 done

--- StringPractice/Anagrams/src/Solution.java
+++ StringPractice/Anagrams/src/Solution.java
@@ -4,6 +4,9 @@ import java.util.*;
 public class Solution {
 
     static boolean isAnagram(String A, String B) {
+        if (A.length() != B.length()) {
+            return false;
+        }
         HashMap<Character, Integer> hashMapA = new HashMap<>();
         HashMap<Character, Integer> hashMapB = new HashMap<>();
         for (int i = 0; i < A.length(); i++) {
@@ -11,16 +14,20 @@ public class Solution {
             char lowerB = Character.toLowerCase(B.charAt(i));
             if (!hashMapA.containsKey(lowerA)) {
                 hashMapA.put(lowerA, 1);
-            } else if (!hashMapB.containsKey(lowerB)) {
-                hashMapB.put(lowerB, 1);
+            } else {
+                hashMapA.put(lowerA, hashMapA.get(lowerA) + 1);
             }
-//            hashMapA.put(lowerA, hashMapA.get(lowerA) + 1);
-//            hashMapB.put(lowerB, hashMapB.get(lowerB) + 1);
-//todo create method to check if key in hash
+            if (!hashMapB.containsKey(lowerB)) {
+                hashMapB.put(lowerB, 1);
+            } else {
+                hashMapB.put(lowerB, hashMapB.get(lowerB) + 1);
+            }
         }
-        System.out.println(hashMapA.values());
-        System.out.println(hashMapB.values());
-
+        for (char key : hashMapA.keySet()) {
+            if (!Objects.equals(hashMapA.get(key), hashMapB.get(key))) {
+                return false;
+            }
+        }
         return true;
     }
 
