c599efa6c5eb63fbd44f20ae7740ade4415ecca9
alohageck0 <royalfiish@gmail.com>
2016-02-03 22:19:47 -0800

    regex

---
+++ Regex/src/Solution.java
@@ -0,0 +1,23 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class myRegex {
+    String pattern = "((\\d{1,2}|1\\d{1,2}|2[0-4][0-9]|25[0-5])\\.){3}(\\d{1,2}|1\\d{1,2}|2[0-4][0-9]|25[0-5])";
+
+    public myRegex() {
+
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String IP = in.next();
+            System.out.println(IP.matches(new myRegex().pattern));
+        }
+
+    }
+}
