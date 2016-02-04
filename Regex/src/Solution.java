08ffd996359d548fe1e17c519718010ad9c066ba
Evgenii <royalfiish@gmail.com>
2016-02-04 09:15:58 -0800

    02/04/2016

--- Regex/src/Solution.java
+++ Regex/src/Solution.java
@@ -3,7 +3,7 @@ import java.util.regex.Pattern;
 import java.util.Scanner;
 
 class myRegex {
-    String pattern = "((\\d{1,2}|1\\d{1,2}|2[0-4][0-9]|25[0-5])\\.){3}(\\d{1,2}|1\\d{1,2}|2[0-4][0-9]|25[0-5])";
+    String pattern = "((0\\d{1,2}|\\d{1,2}|1\\d{1,2}|2[0-4][0-9]|25[0-5])\\.){3}(0\\d{1,2}|\\d{1,2}|1\\d{1,2}|2[0-4][0-9]|25[0-5])";
 
     public myRegex() {
 
