65eb0851e3d1b76716c93166065ee5d942e0e52c
Evgenii <royalfiish@gmail.com>
2016-02-10 09:33:02 -0800

    02/10/2016

--- ContentExtractor/src/TestRegex.java
+++ ContentExtractor/src/TestRegex.java
@@ -3,11 +3,11 @@ import java.util.regex.Pattern;
 
 public class TestRegex {
     public static void main(String[] args) {
+        String strin1 = "<a><b>dsfdsfds</b></adas><cdsad>dadasd</c>";
         String tempStrin = "<a><b>dsfdsfds</b></a>";
-        String strin1 = "<a><b>dsfdsfds</b></a><c>dadasd</c>";
-        System.out.println(tempStrin.matches("</.+><.+>"));
-        System.out.println(strin1.matches("</a><c>"));
-        String regex = "</a><c>";
+//        System.out.println(tempStrin.matches("</.+><.+>"));
+//        System.out.println(strin1.matches("</a><c>"));
+        String regex = "</.+><[^/].+>"; //gotcha!
         Pattern p = Pattern.compile(regex);//todo find regex for this case
         Matcher m = p.matcher(strin1);
         Matcher z = p.matcher(tempStrin);
