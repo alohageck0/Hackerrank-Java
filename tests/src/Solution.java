ccadd82e305f95b518a7240e303d18f846a5ba0d
alohageck0 <royalfiish@gmail.com>
2016-02-10 20:35:58 -0800

    02/10/2016 without regex

--- tests/src/Solution.java
+++ tests/src/Solution.java
@@ -1,75 +1,28 @@
+import java.io.*;
 import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-//<h1>Nayeem loves counseling</h1>
-//(<[a-zA-Z ]+(\d)?>)
+//4
+//        <h1>Nayeem loves counseling</h1>
+//        <h1><h1>Sanjay has no watch</h1></h1><par>So wait for a while</par>
+//        <Amee>safat codes like a ninja</amee>
+//        <SA premium>Imtiaz has a secret crush</SA premium>
+
 public class Solution {
-    private static boolean tagsCorrect(Queue<String> openTagsQueue, Stack<String> closeTagsStack) {
-        while (!openTagsQueue.isEmpty()) {
-            if (!openTagsQueue.remove().equals(closeTagsStack.pop())) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static Queue<String> findOpenTags(Queue<String> queue, String strin) {
-        String tempStrin = strin;
-        while (strin.charAt(0) == '<') {
-            queue.add(tempStrin.substring(1, tempStrin.indexOf('>')));
-//            System.out.println(tempStrin.substring(tempStrin.indexOf('>') + 1));
-            return findOpenTags(queue, tempStrin.substring(tempStrin.indexOf('>') + 1));
-        }
-        return queue;
-    }
-
-    public static String deleteOpenTags(Queue<String> tags, String strin) {
-        String temp = strin;
-        Queue<String> tempTags = tags;
-        while (!tags.isEmpty()) {
-//            strin.replaceFirst("<" + tags.remove() + ">", strin);
-//            System.out.println(tempTags.peek());
-            temp = temp.replaceAll("<" + tempTags.remove() + ">", "");
-//            System.out.println(temp);
-            return deleteOpenTags(tempTags, temp);
-        }
-        return temp;
-    }
-
-    public static Stack<String> findCloseTags(Stack<String> stack, String strin) {
-        String tempStrin = strin;
-        while (strin.length() != 0 && strin.charAt(strin.length() - 1) == '>') {
-            stack.add(tempStrin.substring(tempStrin.indexOf("</") + 2, tempStrin.indexOf('>')));
-            return findCloseTags(stack, tempStrin.substring(tempStrin.indexOf('>') + 1));
-        }
-        return stack;
-    }
-
-    public static String deleteCloseTags(Stack<String> tags, String strin) {
-        String temp = strin;
-        Stack<String> tempTags = tags;
-        while (!tags.isEmpty()) {
-//            strin.replaceFirst("<" + tempTags.pop() + ">", strin);
-            System.out.println(tempTags.peek());
-            temp = temp.replaceAll("</" + tempTags.pop() + ">", "");
-            System.out.println(temp);
-            return deleteCloseTags(tempTags, temp);
-        }
-        return temp;
-    }
-
-
     public static void main(String[] args) {
-        Queue<String> openTags = new LinkedList<>();
-        Stack<String> closeTags = new Stack<>();
-        String test = "<a><b>dsfdsfds</b></a>";
-        findOpenTags(openTags, test);
-        String test1 = deleteOpenTags(openTags, test);
-        System.out.println(test1);
-        findCloseTags(closeTags, test1);
 
-        System.out.println(deleteCloseTags(closeTags, test1));
-//        if (tagsCorrect(openTags, closeTags)) {
-//
-//        }
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while (testCases > 0) {
+            String line = in.nextLine();
+            int start = line.indexOf('<');
+            int end = line.indexOf('>', start);
+            String tag = line.substring(start + 1, end);
+            System.out.println(tag);
+            testCases--;
+        }
     }
 }
+
