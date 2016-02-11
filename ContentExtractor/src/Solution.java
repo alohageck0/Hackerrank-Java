7613ea45c9135ae27ac8c89de12fb6fcf7903aaa
Evgenii <royalfiish@gmail.com>
2016-02-11 09:27:42 -0800

    02/11/2016 Tags done

--- ContentExtractor/src/Solution.java
+++ ContentExtractor/src/Solution.java
@@ -1,76 +1,40 @@
+import java.io.*;
 import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
-    private static boolean tagsCorrect(Queue<String> openTagsQueue, Stack<String> closeTagsStack) {
-        while (!openTagsQueue.isEmpty()) {
-            if (!openTagsQueue.remove().equals(closeTagsStack.pop())) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static Queue<String> findOpenTags(Queue<String> queue, String strin) {
-        String tempStrin = strin;
-        while (strin.charAt(0) == '<') {
-            queue.add(tempStrin.substring(1, tempStrin.indexOf('>')));
-//            System.out.println(tempStrin.substring(tempStrin.indexOf('>') + 1));
-            return findOpenTags(queue, tempStrin.substring(tempStrin.indexOf('>') + 1));
-        }
-        return queue;
-    }
-
-    public static String deleteOpenTags(Queue<String> tags, String strin) {
-        String temp = strin;
-        Queue<String> tempTags = tags;
-        while (!tags.isEmpty()) {
-//            strin.replaceFirst("<" + tags.remove() + ">", strin);
-//            System.out.println(tempTags.peek());
-            temp = temp.replaceAll("<" + tempTags.remove() + ">", "");
-//            System.out.println(temp);
-            return deleteOpenTags(tempTags, temp);
-        }
-        return temp;
-    }
-
-    public static Stack<String> findCloseTags(Stack<String> stack, String strin) {
-        String tempStrin = strin;
-        while (strin.length() != 0 && strin.charAt(strin.length() - 1) == '>') {
-            stack.add(tempStrin.substring(tempStrin.indexOf("</") + 2, tempStrin.indexOf('>')));
-            return findCloseTags(stack, tempStrin.substring(tempStrin.indexOf('>') + 1));
-        }
-        return stack;
-    }
-
-    public static String deleteCloseTags(Stack<String> tags, String strin) {
-        String temp = strin;
-        Stack<String> tempTags = tags;
-        while (!tags.isEmpty()) {
-            temp = temp.replaceAll("</" + tempTags.pop() + ">", "");
-            return deleteCloseTags(tempTags, temp);
-        }
-        return temp;
-    }
-
-
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
         int testCases = Integer.parseInt(in.nextLine());
         while (testCases > 0) {
             String line = in.nextLine();
-            Queue<String> openTags = new LinkedList<>();
-            Stack<String> closeTags = new Stack<>();
-            findOpenTags(openTags, line);
-            String test1 = deleteOpenTags(openTags, line);
-            findCloseTags(closeTags, test1);
-            String result = deleteCloseTags(closeTags, test1);
-            if (tagsCorrect(openTags, closeTags)) {
-                System.out.println(result);
-            } else {
-                System.out.println("None");
-            }
+            int endOpenTag = 0;
+            while (true) {
+                if (endOpenTag >= line.length()) {
+                    break;
+                }
+                int startOpenTag = line.indexOf('<', endOpenTag);
+                endOpenTag = line.indexOf('>', startOpenTag);
 
+                String openTag = line.substring(startOpenTag + 1, endOpenTag);
+                int startCloseTag = line.indexOf('<', endOpenTag);
+                int endCloseTag = line.indexOf('>', startCloseTag);
+                if (line.charAt(startCloseTag + 1) != '/') {
+                    endOpenTag = startCloseTag;
+                    continue;
+                } else if (openTag.equals(line.substring(startCloseTag + 2, endCloseTag))) {
+                    System.out.println(line.substring(endOpenTag + 1, startCloseTag));
+                    endOpenTag = endCloseTag + 1;
+                }else if(!openTag.equals(line.substring(startCloseTag + 2, endCloseTag))){
+                    System.out.println("None");
+                    endOpenTag = endCloseTag + 1;
+                }
+            }
             testCases--;
         }
     }
 }
+
