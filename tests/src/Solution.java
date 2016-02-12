36e7c889ecd508662117110468cbdc5592e04f7f
alohageck0 <royalfiish@gmail.com>
2016-02-11 20:57:04 -0800

    02/11/2016 tags in proces

--- tests/src/Solution.java
+++ tests/src/Solution.java
@@ -4,13 +4,6 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-//4
-//        <h1>Nayeem loves counseling</h1>
-//        <h1><h1>Sanjay has no watch</h1></h1><par>So wait for a while</par>
-//        <Amee>safat codes like a ninja</amee>
-//        <SA premium>Imtiaz has a secret crush</SA premium>
-
-
 
 public class Solution {
     public static void main(String[] args) {
@@ -20,27 +13,43 @@ public class Solution {
         while (testCases > 0) {
             String line = in.nextLine();
             int end = 0;
+            boolean ifNone = false;
             while (true) {
-                if (end >= line.length()) {
+                if (end < 0) {
                     break;
                 }
                 int start = line.indexOf('<', end);
-                end = line.indexOf('>', start);
 
+
+                end = line.indexOf('>', start);
+                if (end < 0) {
+                    ifNone = true;
+                }
                 String openTag = line.substring(start + 1, end);
-//            System.out.println(openTag);
+//                System.out.println(openTag);
                 int startCloseTag = line.indexOf('<', end);
+//                System.out.println("Startclose" + startCloseTag);
                 int endCloseTag = line.indexOf('>', startCloseTag);
+
+//                System.out.println("Endclose" + startCloseTag);
+
                 if (line.charAt(startCloseTag + 1) != '/') {
                     end = startCloseTag;
                     continue;
                 } else if (openTag.equals(line.substring(startCloseTag + 2, endCloseTag))) {
-                    System.out.println(line.substring(end + 1, startCloseTag));
+                    String result = line.substring(end + 1, startCloseTag);
+                    if (result.equals("")) {
+                        ifNone = true;
+                    } else {
+                        System.out.println(line.substring(end + 1, startCloseTag));
+                    }
                     end = endCloseTag + 1;
-                }else if(!openTag.equals(line.substring(startCloseTag + 2, endCloseTag))){
-                    System.out.println("None");
-                    end = endCloseTag + 1;
+                } else {
+                    ifNone = true;
                 }
+            }
+            if (ifNone) {
+                System.out.println("None");
             }
             testCases--;
         }
