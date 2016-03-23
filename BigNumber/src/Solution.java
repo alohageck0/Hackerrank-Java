43b64de511524887efb61aac012a86141407bf58
alohageck0 <royalfiish@gmail.com>
2016-03-22 22:34:32 -0700

    home practice 03/22/16

---
+++ BigNumber/src/Solution.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Solution {
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      BigInteger A = new BigInteger(in.nextLine());
+      BigInteger B = new BigInteger(in.nextLine());
+
+      System.out.println(A.add(B));
+      System.out.println(A.multiply(B));
+   }
+}
