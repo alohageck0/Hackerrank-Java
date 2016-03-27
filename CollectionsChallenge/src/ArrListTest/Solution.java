a57404d35328ed6659be590f12c2156c433bf27a
alohageck0 <royalfiish@gmail.com>
2016-03-27 16:01:57 -0700

    home practice 03/26/16

---
+++ CollectionsChallenge/src/ArrListTest/Solution.java
@@ -0,0 +1,31 @@
+package ArrListTest;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution {
+   public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      ArrayList<int[]> arrayList = new ArrayList<>();
+      int n = scanner.nextInt();
+      for (int i = 0; i < n; i++) {
+         int d = scanner.nextInt();
+         int[] arr = new int[d];
+         for (int j = 0; j < d; j++) {
+            arr[j] = scanner.nextInt();
+         }
+         arrayList.add(arr);
+      }
+      int q = scanner.nextInt();
+      for (int i = 0; i < q; i++) {
+         int x = scanner.nextInt();
+         int y = scanner.nextInt();
+         int[] line = arrayList.get(x-1);
+         try {
+            System.out.println(line[y-1]);
+         } catch (Exception e) {
+            System.out.println("ERROR!");
+         }
+      }
+   }
+}
