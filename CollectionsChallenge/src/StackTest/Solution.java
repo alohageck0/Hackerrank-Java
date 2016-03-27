d2c27870311c206da0cb9cf97f8cd7315c66c8ef
alohageck0 <royalfiish@gmail.com>
2016-03-26 20:57:41 -0700

    home practice 03/26/16

---
+++ CollectionsChallenge/src/StackTest/Solution.java
@@ -0,0 +1,18 @@
+package StackTest;
+
+import java.util.*;
+
+class Solution {
+
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+
+      while (sc.hasNext()) {
+         String input = sc.next();
+         //Complete the code
+      }
+
+   }
+}
+
+
