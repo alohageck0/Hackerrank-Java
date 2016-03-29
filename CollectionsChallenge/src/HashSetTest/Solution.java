1b7ee1873b81d0cbce47139ce076fd89153e884b
alohageck0 <royalfiish@gmail.com>
2016-03-28 23:49:32 -0700

    home practice 03/28/16

--- CollectionsChallenge/src/HashSetTest/Solution.java
+++ CollectionsChallenge/src/HashSetTest/Solution.java
@@ -1,7 +1,55 @@
 package HashSetTest;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+class Pair {
+   private String firstPersonName;
+   private String secondPersonName;
+
+   Pair(String firstPersonName, String secondPersonName) {
+      this.firstPersonName = firstPersonName;
+      this.secondPersonName = secondPersonName;
+   }
+
+   private String getFirstPersonName() {
+      return firstPersonName;
+   }
+
+   private String getSecondPersonName() {
+      return secondPersonName;
+   }
+
+   @Override
+   public int hashCode() {
+      return this.firstPersonName.hashCode() + this.secondPersonName.hashCode() + 44;
+   }
+
+   @Override
+   public boolean equals(Object obj) {
+      boolean result = false;
+      if (obj instanceof Pair) {
+         Pair pair = (Pair) obj;
+         if (this.firstPersonName.equals(pair.getFirstPersonName())) {
+            result = this.secondPersonName.equals(pair.getSecondPersonName());
+         }
+      }
+      return result;
+   }
+}
+
 public class Solution {
-    public static void main(String[] args) {
-        //fdf
-    }
+   public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      int n = scanner.nextInt();
+      Set<Pair> set = new HashSet<>();
+      scanner.nextLine();
+      for (int i = 0; i < n; i++) {
+         String s = scanner.nextLine();
+         Pair pair = new Pair(s.substring(0, s.indexOf(' ')), s.substring(s.indexOf(' ') + 1));
+         set.add(pair);
+         System.out.println(set.size());
+      }
+   }
 }
