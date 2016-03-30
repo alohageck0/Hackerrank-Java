c2d0d8c1c4be0cc8b9b890a48b8eb25ae0d867e3
alohageck0 <royalfiish@gmail.com>
2016-03-29 22:00:17 -0700

    home practice 03/29/16

--- CollectionsChallenge/src/DequeueTest/Solution.java
+++ CollectionsChallenge/src/DequeueTest/Solution.java
@@ -1,7 +1,12 @@
 package DequeueTest;
 
+import java.util.Scanner;
+
 public class Solution {
-    public static void main(String[] args) {
-//fdf
-    }
+   public static void main(String[] args) {
+
+      Scanner scanner = new Scanner(System.in);
+      int n = scanner.nextInt();
+      int subarrayLength = scanner.nextInt();
+   }
 }
