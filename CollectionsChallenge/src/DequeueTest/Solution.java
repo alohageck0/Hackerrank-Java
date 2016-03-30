e1cf3bdfbb8aea9dc5e4650603ef457ef61ca6dc
Evgenii <royalfiish@gmail.com>
2016-03-30 11:05:06 -0700

    work practice 03/30/16

--- CollectionsChallenge/src/DequeueTest/Solution.java
+++ CollectionsChallenge/src/DequeueTest/Solution.java
@@ -3,33 +3,45 @@ package DequeueTest;
 import java.util.*;
 
 public class Solution {
-   public static void main(String[] args) {
+    public static void main(String[] args) {
 
-      Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-      int n = scanner.nextInt();
-      int subarrayLength = scanner.nextInt();
-      Deque<Integer> deque = new ArrayDeque<>();
-      int maxNumberUniqueInt = 0;
+        int n = scanner.nextInt();
+        int subarrayLength = scanner.nextInt();
+        Deque<Integer> deque = new ArrayDeque<>();
+        Set<Integer> uniqueNumbers = new HashSet<>(subarrayLength);
+        Map<Integer, Integer> countNumbers = new HashMap<>(subarrayLength);
+        int maxNumberUniqueInt = 0;
 
-      for (int i = 0; i < n; i++) {
-         deque.add(scanner.nextInt());
-      }
 
-      while (deque.size() >= subarrayLength) {
-          if (maxNumberUniqueInt>deque.size()){
-              break;
-          }
-         Set<Integer> set = new HashSet<>();
-         Iterator<Integer> iterator = deque.iterator();
-         for (int i = 0; i < subarrayLength; i++) {
-            set.add(iterator.next());
-         }
-         deque.removeFirst();
-         if (set.size() > maxNumberUniqueInt) {
-            maxNumberUniqueInt = set.size();
-         }
-      }
-      System.out.println(maxNumberUniqueInt);
-   }
+        for (int i = 0; i < n; i++) {
+            int number = scanner.nextInt();
+            Integer countVal;
+
+            if (deque.size() == subarrayLength) {
+                Integer numberHeadOfDeque = deque.remove();
+                countVal = countNumbers.get(numberHeadOfDeque);
+                countNumbers.put(numberHeadOfDeque, countVal - 1);
+                if (countVal - 1 == 0) {
+                    uniqueNumbers.remove(numberHeadOfDeque);
+                }
+            }
+
+            deque.add(number);
+            countVal = countNumbers.get(number);
+            if (countVal == null) {
+                countVal = 0;
+            }
+            countNumbers.put(number, countVal + 1);
+            uniqueNumbers.add(number);
+
+            if (deque.size() == subarrayLength) {
+                maxNumberUniqueInt = Math.max(maxNumberUniqueInt, uniqueNumbers.size());
+            }
+
+        }
+
+        System.out.println(maxNumberUniqueInt);
+    }
 }
