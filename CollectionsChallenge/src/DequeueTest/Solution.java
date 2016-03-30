d5a140d693a7dc917528981745d98f20a1f9e5b4
alohageck0 <royalfiish@gmail.com>
2016-03-30 00:24:10 -0700

    home practice 03/30/16

--- CollectionsChallenge/src/DequeueTest/Solution.java
+++ CollectionsChallenge/src/DequeueTest/Solution.java
@@ -1,20 +1,32 @@
 package DequeueTest;
 
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
    public static void main(String[] args) {
 
       Scanner scanner = new Scanner(System.in);
+
       int n = scanner.nextInt();
       int subarrayLength = scanner.nextInt();
-      Deque<Integer> deque = new LinkedList<>();
+      Deque<Integer> deque = new ArrayDeque<>();
+      int maxNumberUniqueInt = 0;
 
       for (int i = 0; i < n; i++) {
          deque.add(scanner.nextInt());
       }
 
+      while (deque.size() >= subarrayLength) {
+         Set<Integer> set = new HashSet<>();
+         Iterator<Integer> iterator = deque.iterator();
+         for (int i = 0; i < subarrayLength; i++) {
+            set.add(iterator.next());
+         }
+         deque.removeFirst();
+         if (set.size() > maxNumberUniqueInt) {
+            maxNumberUniqueInt = set.size();
+         }
+      }
+      System.out.println(maxNumberUniqueInt);
    }
 }
