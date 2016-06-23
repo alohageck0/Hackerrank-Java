3307156828f946ddba585eb85fa81699021a09b4
alohageck0 <royalfiish@gmail.com>
2016-06-23 00:15:27 -0700

    home 06/23/16

--- tests/src/InterviewQuestions/ProductOfInts.java
+++ tests/src/InterviewQuestions/ProductOfInts.java
@@ -5,11 +5,15 @@ import java.util.Arrays;
 public class ProductOfInts {
    public static void main(String[] args) {
 
-      int[] arr = {1, 2, 3, 4, 5};
+      int[] arr = {1, 2, 6, 5, 9};
       System.out.println(Arrays.toString(getProductsOfAllIntsExceptAtIndex(arr)));
+      System.out.println(Arrays.toString(getProductsOfAllIntsExceptAtIndex1(arr)));
    }
 
+   //brute force
    static int[] getProductsOfAllIntsExceptAtIndex(int[] arr) {
+      long startTime = System.nanoTime();
+
       int[] arrProducts = new int[arr.length];
       for (int i = 0; i < arr.length; i++) {
          arrProducts[i] = 1;
@@ -19,6 +23,28 @@ public class ProductOfInts {
             }
          }
       }
+      long stopTime = System.nanoTime();
+      System.out.println(stopTime - startTime);
       return arrProducts;
    }
+
+   //greddy
+   static int[] getProductsOfAllIntsExceptAtIndex1(int[] arr) {
+      long startTime = System.nanoTime();
+
+      int[] arrProducts = new int[arr.length];
+      int beforeMultiplied = 1;
+      int afterMultiplied = 1;
+      for (int i = 0; i < arr.length; i++) {
+         for (int j = 0; j < arr.length; j++) {
+
+
+         }
+      }
+      long stopTime = System.nanoTime();
+      System.out.println(stopTime - startTime);
+      return arrProducts;
+
+   }
+
 }
