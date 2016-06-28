bf91011240e95a0117b7cdab7e4c5b6896e67931
alohageck0 <royalfiish@gmail.com>
2016-06-28 00:36:02 -0700

    home 06/28/16

--- tests/src/ConvertStringToInt/SolutionTestng.java
+++ tests/src/ConvertStringToInt/SolutionTestng.java
@@ -3,25 +3,21 @@ package ConvertStringToInt;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-// Unit testing frameworks
-
-/*
- * To execute Java, please define "static void main" on a class
- * named Solution.
- *
- * If you need more classes, simply define them inline.
- */
-
-public class Solution {
+public class SolutionTestng {
    @Test
    public void testStringToFloat() {
-      Assert.assertEquals(1, stringToFloat("1"), 0.0001);
-      Assert.assertEquals(100, stringToFloat("100"), 0.0001);
+      Assert.assertEquals(stringToFloat("1"), 1, 0.0001);
+      Assert.assertEquals(stringToFloat("100"), 100, 0.0001);
+      Assert.assertEquals(stringToFloat("100.44"), 100.44, 0.0001);
+      Assert.assertEquals(stringToFloat("100.4499999"), 100.4499999, 0.0001);
+      Assert.assertEquals(stringToFloat("-100.4499999"), -100.4499999, 0.0001);
+      Assert.assertNull(stringToFloat("-100.4499dd999"));
 
       Assert.assertEquals(new Integer(1), stringToInt("1"));
       Assert.assertEquals(new Integer(100), stringToInt("100"));
-      // Assert.assertNull(stringToInt("letters"));
+      Assert.assertNull(stringToInt("letters"));
       Assert.assertEquals(new Integer(-20), stringToInt("-20"));
+      Assert.assertNull(stringToInt("-20l"));
 
    }
 
@@ -31,8 +27,27 @@ public class Solution {
     * @param input - String that should be converted
     * @return float - Float of input string number
     */
-   public float stringToFloat(String input) {
-      return stringToInt(input);
+   public Float stringToFloat(String input) {
+      String[] parts = input.split("\\.");
+      if (parts.length == 2) {
+         int decimals = parts[1].length();
+
+         float wholePart = 0;
+         float decimalPart = 0;
+         try {
+            wholePart = stringToInt(parts[0]);
+            decimalPart = (float) (stringToInt(parts[1]) * Math.pow(10, -decimals));
+            if (wholePart < 0) {
+               return wholePart - decimalPart;
+            } else {
+               return wholePart + decimalPart;
+            }
+         } catch (Exception e) {
+            return null;
+         }
+      }
+
+      return (float) stringToInt(input);
    }
 
    /**
@@ -43,7 +58,7 @@ public class Solution {
     */
    public Integer stringToInt(String input) {
       int minus = 1;
-      if(input.charAt(0)=='-'){
+      if (input.charAt(0) == '-') {
          minus = -1;
          input = input.substring(1);
       }
@@ -53,17 +68,17 @@ public class Solution {
       int stringLength = input.length();
 
       //100
-      for (int i = stringLength - 1; i != -1; i--){
+      for (int i = stringLength - 1; i != -1; i--) {
 
 
          char singleChar = input.charAt(i);
          //0
          int singleDigit;
 
-         try{
+         try {
             singleDigit = Integer.parseInt(Character.toString(singleChar));
             // int 0 - int 0
-         } catch (Exception e) {
+         } catch (NumberFormatException e) {
             return null;
          }
          // Find which "place" we are in the string and multiply by
@@ -72,16 +87,6 @@ public class Solution {
          returnInt += singleDigit * Math.pow(10, stringLength - i - 1);
 
       }
-      return returnInt*minus;
-   }
-
-   public static void main(String[] args) {
-//      JUnitCore.main("Solution");
-
-      Solution solution = new Solution();
-
-      System.out.print(solution.stringToInt("letters"));
+      return returnInt * minus;
    }
 }
-
-
