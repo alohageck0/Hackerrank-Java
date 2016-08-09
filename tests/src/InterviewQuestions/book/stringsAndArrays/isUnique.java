b601714f91aa64568723723dd7e90d8b4f704c05
alohageck0 <royalfiish@gmail.com>
2016-08-08 21:23:44 -0700

    home 08/08/16

---
+++ tests/src/InterviewQuestions/book/stringsAndArrays/isUnique.java
@@ -0,0 +1,30 @@
+package InterviewQuestions.book.stringsAndArrays;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+public class isUnique {
+
+
+   @Test
+   public void test(){
+      assertTrue(isUnique("azxcvbmret"));
+      assertFalse(isUnique("asasddc"));
+   }
+
+   public boolean isUnique(String input) {
+      if (input.length() > 128) {
+         return false;
+      }
+      boolean[] asciiCodeArray = new boolean[128];
+      for (int i = 0; i < input.length(); i++) {
+         int asciiCode = input.charAt(i);
+         if (asciiCodeArray[asciiCode]) {
+            return false;
+         } else {
+            asciiCodeArray[asciiCode] = true;
+         }
+      }
+      return true;
+   }
+}
