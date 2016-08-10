99908eb2afc9f155026d3a7479d470de25925b83
alohageck0 <royalfiish@gmail.com>
2016-08-09 21:07:31 -0700

    home 08/08/16

---
+++ tests/src/InterviewQuestions/book/stringsAndArrays/HashTable.java
@@ -0,0 +1,11 @@
+package InterviewQuestions.book.stringsAndArrays;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import static javafx.scene.input.KeyCode.T;
+
+public class HashTable<T> {
+   private ArrayList<LinkedList<T>> arrayOfLinkedLists = new ArrayList<>();
+
+}
