239c7b6aca5ec109e88f433969e89a09dc6aaa5d
alohageck0 <royalfiish@gmail.com>
2016-08-13 18:54:36 -0700

    home 08/13/16

---
+++ tests/src/InterviewQuestions/book/stringsAndArrays/StringCompression.java
@@ -0,0 +1,38 @@
+package InterviewQuestions.book.stringsAndArrays;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class StringCompression {
+
+   @Test
+   public void test() {
+      Assert.assertEquals(compressString("aabcccccaaa"), "a2b1c5a3");
+   }
+
+   public String compressString(String input) {
+      StringBuilder compressedString = new StringBuilder();
+      int counter = 0;
+      if (input.length() > 0) {
+         String partiallyCompresed = "";
+         char lastChar = input.charAt(input.length() - 1);
+         for (int i = input.length() - 1; i >= 0; i--) {
+            if (input.charAt(i) == lastChar) {
+               counter++;
+            } else {
+               break;
+            }
+         }
+         partiallyCompresed = lastChar + String.valueOf(counter);
+         return compressString(input.substring(0, input.length() - counter)) + partiallyCompresed;
+      }
+      String output = compressedString.toString();
+      if (output.length() > input.length()) {
+         return input;
+      } else {
+         return output;
+      }
+   }
+
+
+}
