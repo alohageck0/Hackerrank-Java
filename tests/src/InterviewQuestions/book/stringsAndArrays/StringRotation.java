886eb3bab98b7f2abf97b739edb0f53300cc2624
alohageck0 <royalfiish@gmail.com>
2016-08-21 19:20:11 -0700

    home 08/21/16

--- tests/src/InterviewQuestions/book/stringsAndArrays/StringRotation.java
+++ tests/src/InterviewQuestions/book/stringsAndArrays/StringRotation.java
@@ -1,16 +1,31 @@
 package InterviewQuestions.book.stringsAndArrays;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class StringRotation {
 
    @Test
    public void test() {
+      Assert.assertTrue(isRoatation("waterbottle", "erbottlewat"));
 
    }
 
-   public boolean isSubstring() {
+   public boolean isRoatation(String s1, String s2) {
+      int len = s1.length();
+      if (s2.length() == len && len > 0) {
+         String s1s1 = s1 + s1;
+         return isSubstring(s1s1, s2);
+      }
+      return false;
 
-      return true;
+   }
+
+   public boolean isSubstring(String s1, String s2) {
+      if (s1.contains(s2)) {
+         return true;
+      } else {
+         return false;
+      }
    }
 }
