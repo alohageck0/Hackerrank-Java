ff4b33684b79874c4e4f0c2c667a8b3781c98507
alohageck0 <royalfiish@gmail.com>
2016-08-22 22:30:44 -0700

    home 08/22/16

---
+++ src/implementation/NonDivSubset.java
@@ -0,0 +1,17 @@
+package implementation;
+
+import java.util.*;
+
+public class NonDivSubset {
+
+   //to be continued
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      int len = in.nextInt();
+      int divisor = in.nextInt();
+      List<Integer> numbers = new LinkedList<>();
+      while (in.hasNextInt()) {
+         numbers.add(in.nextInt());
+      }
+   }
+}
