e9ffae96dd0acc34fd0ce7c4a7469717eb651fb1
alohageck0 <royalfiish@gmail.com>
2016-08-24 22:32:54 -0700

    home 08/24/16

--- tests/src/InterviewQuestions/book/stringsAndArrays/PrintDuplicateChars.java
+++ tests/src/InterviewQuestions/book/stringsAndArrays/PrintDuplicateChars.java
@@ -14,15 +14,24 @@ public class PrintDuplicateChars {
 
    @Test
    public void test() {
+      String test = new String();
+      String test1 = "";
+      System.out.println(printDuplicates("sdfsdfjjsdijfsdjfjsdfjsdjfsdnfkjsdnjfnsdnfskdjnf"));
+      Assert.assertEquals(printDuplicates("asdfghjklertyu"), "No duplicates");
+      Assert.assertNull(printDuplicates(test));
+      Assert.assertNull(printDuplicates(test1));
       System.out.println(printDuplicates("sdfsdfjjsdijfsdjfjsdfjsdjfsdnfkjsdnjfnsdnfskdjnf"));
    }
 
    private String printDuplicates(String input) {
+      if (input == null || input.length() < 1) {
+         return null;
+      }
       StringBuilder output = new StringBuilder();
       Map<Character, Integer> characters = new LinkedHashMap<>();
       for (int i = 0; i < input.length(); i++) {
          char key = input.charAt(i);
-         if (!output.toString().contains(String.valueOf(key))){
+         if (!output.toString().contains(String.valueOf(key))) {
             if (!characters.containsKey(key)) {
                characters.put(key, 1);
             } else {
@@ -30,7 +39,13 @@ public class PrintDuplicateChars {
             }
          }
       }
-      return output.toString();
+      String out = output.toString();
+      if (out.length() < 1) {
+         return "No duplicates";
+      } else {
+         return output.toString();
+
+      }
    }
 
 }
