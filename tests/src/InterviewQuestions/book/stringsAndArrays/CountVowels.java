ef40be59977ee83e60274744b07de67343b8a188
alohageck0 <royalfiish@gmail.com>
2016-08-29 00:03:57 -0700

    home 08/29/16

---
+++ tests/src/InterviewQuestions/book/stringsAndArrays/CountVowels.java
@@ -0,0 +1,17 @@
+package InterviewQuestions.book.stringsAndArrays;
+
+import org.testng.annotations.Test;
+
+public class CountVowels {
+
+   @Test
+   public void test() {
+
+   }
+
+   public void countVowels(String input) {
+
+
+
+   }
+}
