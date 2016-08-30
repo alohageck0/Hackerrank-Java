47200d388f3283f51623037d455f70b12b4cf25f
alohageck0 <royalfiish@gmail.com>
2016-08-29 23:30:21 -0700

    home 08/29/16

--- tests/src/InterviewQuestions/book/stringsAndArrays/CountVowels.java
+++ tests/src/InterviewQuestions/book/stringsAndArrays/CountVowels.java
@@ -1,17 +1,41 @@
 package InterviewQuestions.book.stringsAndArrays;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CountVowels {
 
    @Test
    public void test() {
+      Assert.assertEquals(countVowels("aaaa"), 4);
+      Assert.assertEquals(countVowels("aaaaba"), 5);
+      Assert.assertEquals(countVowels("auioeyaba"), 7);
+      Assert.assertEquals(countVowels("bbbbb"), 0);
+
 
    }
 
-   public void countVowels(String input) {
-
-
-
+   public int countVowels(String input) {
+      int count = 0;
+      for (int i = 0; i < input.length(); i++) {
+         switch ((int) input.charAt(i)) {
+            case (int) 'a':
+               count++;
+               break;
+            case (int) 'u':
+               count++;
+               break;
+            case (int) 'i':
+               count++;
+               break;
+            case (int) 'o':
+               count++;
+               break;
+            case (int) 'e':
+               count++;
+               break;
+         }
+      }
+      return count;
    }
 }
