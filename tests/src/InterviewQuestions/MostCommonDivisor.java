a9e0678af51fd23237275685e3c73cad86184d09
alohageck0 <royalfiish@gmail.com>
2016-09-19 22:48:47 -0700

    home 09/19/16

--- tests/src/InterviewQuestions/MostCommonDivisor.java
+++ tests/src/InterviewQuestions/MostCommonDivisor.java
@@ -9,18 +9,30 @@ public class MostCommonDivisor {
       simplify(2, 4);
    }
 
+   //   public void simplify(int numerator, int denominator) {
+//      if (numerator < denominator) {
+//         if (denominator % numerator == 0) {
+//            System.out.printf("%s %s", 1, denominator / numerator);
+//         }
+//      } else {
+//         if (numerator % denominator == 0) {
+//            System.out.printf("%s %s", 1, numerator / denominator);
+//         }
+//      }
+//      int maxDiv;
+//      if (numerator < denominator) {
+//         maxDiv = denominator / numerator;
+//      } else {
+//         maxDiv = numerator / denominator;
+//      }
+//      for (int i = maxDiv;i>)
+//
+//   }
    public void simplify(int numerator, int denominator) {
-      if (numerator < denominator) {
-         if (denominator % numerator == 0) {
-            System.out.printf("%s %s", 1, denominator / numerator);
-         }
-      } else {
-         if (numerator % denominator == 0) {
-            System.out.printf("%s %s", 1, numerator / denominator);
-         }
+      while (denominator!=0){
+         int temp = denominator;
+         denominator = temp;
       }
-//      int maxDiv = denominator / numerator;
-
-
    }
+
 }
