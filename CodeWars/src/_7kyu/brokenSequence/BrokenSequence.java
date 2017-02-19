dd8f449b16de23491e5da20761aeff2e98ad06ca
alohageck0 <royalfiish@gmail.com>
2017-02-18 23:37:52 -0800

    home 02/18/17

--- CodeWars/src/_7kyu/brokenSequence/BrokenSequence.java
+++ CodeWars/src/_7kyu/brokenSequence/BrokenSequence.java
@@ -6,25 +6,25 @@ import java.util.Collections;
 public class BrokenSequence {
    public int findMissingNumber(String sequence) {
       int missing = 0;
-      char[] sequenceInIntArray;
-      Collections.sort(Arrays.asList(sequence));
       try {
          if (sequence.length() == 0) {
             return 0;
          }
          int sequenceInInt = Integer.parseInt(sequence.replace(" ", ""));
-         sequenceInIntArray = sequence.toCharArray();
       } catch (Exception e) {
          return 1;
       }
+      String[] sequenceInIntArray = sequence.split(" ");
+      Collections.sort(Arrays.asList(sequenceInIntArray));
       if (sequence.charAt(sequence.length() - 1) == sequence.length()) {
          return 0;
       } else {
          for (int index = 1; index < sequenceInIntArray.length; index++) {
-            int nextInt = Character.getNumericValue(sequenceInIntArray[index]);
-            int currentInt = Character.getNumericValue(sequenceInIntArray[index - 1]);
+            int nextInt = Integer.parseInt(sequenceInIntArray[index]);
+            int currentInt = Integer.parseInt(sequenceInIntArray[index - 1]);
             if ((nextInt - currentInt) > 1) {
                missing = nextInt - 1;
+               break;
             }
          }
       }
