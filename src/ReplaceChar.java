897f39f4074f573e5454e76c4255328e1aabe848
alohageck0 <royalfiish@gmail.com>
2017-02-18 20:21:06 -0800

    home 02/18/17

--- src/ReplaceChar.java
+++ src/ReplaceChar.java
@@ -8,10 +8,19 @@ public class ReplaceChar {
       String inputNull = null;
       String inputEmpty = "";
       Assert.assertEquals(replace("asdfghjkl", 'f'), "asdghjkl");
+      Assert.assertEquals(replaceRec("asdfghjkl", 'f'), "asdghjkl");
+      Assert.assertEquals(replaceRec("aaaaaaa", 'f'), "aaaaaaa");
       Assert.assertEquals(replace("aaaaaaa", 'a'), "");
+      Assert.assertEquals(replaceRec("aaaaaaa", 'a'), "");
+      Assert.assertEquals(replaceRec("abababb", 'a'), "bbbb");
       Assert.assertEquals(replace("a", 'a'), "");
+      Assert.assertEquals(replaceRec("a", 'a'), "");
       Assert.assertEquals(replace(inputNull, 'a'), null);
+      Assert.assertEquals(replaceRec(inputNull, 'a'), null);
       Assert.assertEquals(replace(inputEmpty, 'a'), "");
+      Assert.assertEquals(replaceRec(inputEmpty, 'a'), "");
+
+
    }
 
    public String replace(String input, char character) {
@@ -31,6 +40,27 @@ public class ReplaceChar {
       return result.toString();
    }
 
-
+   public String replaceRec(String input, char ch) {
+      if (input == null) {
+         return null;
+      }
+      if (input.equals("")) {
+         return "";
+      }
+      char c = input.charAt(0);
+      if (input.length() == 1) {
+         if (c == ch) {
+            return "";
+         } else {
+            return input;
+         }
+      } else {
+         if (c == ch) {
+            return replaceRec(input.substring(1), ch);
+         } else {
+            return c + replaceRec(input.substring(1), ch);
+         }
+      }
+   }
 
 }
