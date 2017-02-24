181247d151d430a6834bef3e7be45953daf3bbc0
alohageck0 <royalfiish@gmail.com>
2017-02-24 02:03:56 -0800

    home 02/23/17 prevPalindrom fixed

--- src/EvernoteF2F.java
+++ src/EvernoteF2F.java
@@ -4,9 +4,9 @@ public enum EvernoteF2F {
    ACTIVE, INACTIVE, PENDING;
 
    public static void main(String[] args) {
-      System.out.println(EvernoteF2F.ACTIVE);
+//      System.out.println(EvernoteF2F.ACTIVE);
 
-      System.out.println(prevPalidrome(113));
+      System.out.println(prevPalindrome(1118883));
       int[] arr = {1, 2, 4, 3, 4, 2, 65};
       HashSet<Integer> set = new HashSet<>();
 
@@ -15,34 +15,37 @@ public enum EvernoteF2F {
       }
       Iterator<Integer> iterator = set.iterator();
 
+   }
 
-      while (iterator.hasNext()) {
-
+   public static int prevPalindrome(int x) {
+      int testIfPalindrom = x - 1;
+      if (ifPalindrom(testIfPalindrom)) {
+         return testIfPalindrom;
+      } else {
+         return prevPalindrome(testIfPalindrom);
       }
    }
 
-   public static boolean prevPalidrome(int x) {
-
+   private static boolean ifPalindrom(int x) {
       boolean ifPalindrome = true;
-
       String intToString = String.valueOf(x);
       if (intToString.length() < 2) {
          return ifPalindrome;
       }
+      Stack<Character> stack = new Stack<>();
+      Queue<Character> queue = new LinkedList<>();
       for (int i = 0; i < intToString.length(); i++) {
-         Stack<Character> stack = new Stack<>();
-         Queue<Character> queue = new LinkedList<>();
-
-
          stack.push(intToString.charAt(i));
          queue.add(intToString.charAt(i));
-
-         if (stack.peek() != queue.peek()) {
+      }
+      while (!stack.empty()) {
+         if (stack.pop() != queue.remove()) {
             ifPalindrome = false;
+            break;
          }
       }
-
       return ifPalindrome;
-
    }
+
+
 }
